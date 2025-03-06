cdba693ddd1a8efd8b98f4fbb4eda7ac9c1b63fc
c++ <fnivicela2008@gmail.com>
2025-03-06 10:56:03 -0500

    Initial Commit

---
+++ AIhealthcoach/AIhealthcoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIhealthcoachApp.swift
+//  AIhealthcoach
+//
+//  Created by Frank Nivicela on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AIhealthcoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIhealthcoachTests/AIhealthcoachTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIhealthcoachTests.swift
+//  AIhealthcoachTests
+//
+//  Created by Frank Nivicela on 3/6/25.
+//
+
+import Testing
+@testable import AIhealthcoach
+
+struct AIhealthcoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
